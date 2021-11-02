14705e327d4dd9662834ae4c4cf4d41f2af2ca8e
Michael Chang <michael@1x-nat-vl932-172-30-209-240.wireless.umass.edu>
2021-11-02 16:26:22 -0400

    copy hw2

---
+++ outputPDF.sh
@@ -0,0 +1,5 @@
+jupyter nbconvert --to pdf FullyConnectedNets.ipynb
+jupyter nbconvert --to pdf BatchNormalization.ipynb
+jupyter nbconvert --to pdf Dropout.ipynb
+jupyter nbconvert --to pdf ConvolutionalNetworks.ipynb
+jupyter nbconvert --to pdf PyTorch.ipynb
